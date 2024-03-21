a85811223afc99facebc1e2f9da16bce90a6a525
Randrianarison-Kevin <randrianarison.noavintsoa.kevin@gmail.com>
2024-03-21 14:57:16 +0300

    Affichage des donnees

---
+++ src/Controller/AfficheController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Test;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AfficheController extends AbstractController
+{
+    #[Route('/affiche', name: 'app_affiche')]
+    public function index(EntityManagerInterface $entityManagerInterface): Response
+    {
+        $repository = $entityManagerInterface->getRepository(Test::class);
+        $test = $repository->findAll();
+        return $this->render('affiche/index.html.twig', [
+            'tests' => $test,
+        ]);
+    }
+}
